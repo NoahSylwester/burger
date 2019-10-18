9f9fe2f89f247acea8851fc2980a18c20fb2a658
Noah Sylwester <nanoabram@comcast.net>
2019-10-18 16:55:36 -0700

    update burgers.js, index.handlebars such that new burgers can be created on client side

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -17,11 +17,9 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  cat.insertOne([
-    "burger_name", "devoured"
-  ], [
-    req.body.name, req.body.sleepy
-  ], function(result) {
+  burger.insertOne(
+    req.body.name,
+    function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
@@ -32,7 +30,7 @@ router.put("/api/burgers/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  cat.update({
+  burger.update({
     sleepy: req.body.sleepy
   }, condition, function(result) {
     if (result.changedRows == 0) {
