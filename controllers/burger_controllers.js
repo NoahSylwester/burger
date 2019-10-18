310d97bccb5ef0c254f311079d65dfba7c38b8db
Noah Sylwester <nanoabram@comcast.net>
2019-10-18 14:47:46 -0700

    add to index.handlebars, burger_controllers.js, server.js

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -5,8 +5,15 @@ var router = express.Router();
 // import model for its functions
 var burger = require('../models/burger.js');
 
+// create routes and logic
 router.get("/", function(req, res) {
-  
+  burger.selectAll(function(receivedData) {
+    var handlebarsObject = {
+      burgers: receivedData
+    };
+    console.log(handlebarsObject);
+    res.render('index', handlebarsObject);
+  })
 });
 
 module.exports = router;
